b8722778dfa8fc482aba3d547d237b9ca9bc4e53
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-08 19:46:25 +0300

    add the reviews routes

--- Routers/Product_Routs.js
+++ Routers/Product_Routs.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
-const broductCtrl = require('../controllers/Product_controllers')
+const productCtrl = require('../controllers/Product_controllers')
+const reviewCtrl = require('../controllers/Review_controllers')
 
-router.post('', broductCtrl.AddingProduct)
-router.get('', broductCtrl.GettingAllProducts)
-router.get('/:id', broductCtrl.GettingOneProduct)
-router.put('/:id', broductCtrl.UpdateProduct)
-router.delete('/:id', broductCtrl.DeletingProduct)
+router.post('', productCtrl.AddingProduct)
+router.get('', productCtrl.GettingAllProducts)
+router.get('/:productId', productCtrl.GettingOneProduct)
+router.put('/:productId', productCtrl.UpdateProduct)
+router.delete('/:productId', productCtrl.DeletingProduct)
+
+//reviews routes
+
+router.get('/:productId/reviews', reviewCtrl.reviews_getAll)
+router.post('/:productId/reviews', reviewCtrl.reviews_create_post)
+router.delete('/:productId/reviews/:reviewId', reviewCtrl.reviews_delete_delete)
 
 module.exports = router
