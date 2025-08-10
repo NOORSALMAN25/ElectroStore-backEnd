82ec4fc4a946d464a4c74cd28adb15bf9026c69d
maryamsali02 <maryamsalah233@gmail.com>
2025-08-10 11:40:02 +0300

    profileget

--- Routers/Profile_Routs.js
+++ Routers/Profile_Routs.js
@@ -1,1 +1,7 @@
-Profile_Routs.js
+const router = require('express').Router()
+const ProfileCtrl = require('../controllers/Profile_controller') 
+  //Calling API'S
+  //we just need the get to show the information and put to allow the user to edit his name or email only
+  router.get('/profile', ProfileCtrl.profile_show_get)
+
+module.exports = router
--- controllers/Profile_controller.js
+++ controllers/Profile_controller.js
@@ -1,1 +1,22 @@
-Profile_controller.js
+const Profile = require('../Routers/Profile_Routs')
+
+//API'S
+const profile_show_get = async (req, res) => {
+  try {
+    console.log('user', req.session.user)
+    res.send('Profile page')
+  } catch (error) {
+    console.log(error)
+    res.status('500').send('Server error')
+  }
+}
+// it dosnt shows the informationn of the user at the database it just print information
+
+// const profile_update_put = async (req, res) => {
+
+// }
+
+module.exports = {
+  profile_show_get,
+  profile_update_put
+}
