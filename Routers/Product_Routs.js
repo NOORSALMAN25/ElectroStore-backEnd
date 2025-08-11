e5b3d5366649bcdc384adb67a97d44cf0b2f0a62
mad6717 <mad6717@gmail.com>
2025-08-11 09:19:40 +0300

    spilling mistakes

--- Routers/Product_Routs.js
+++ Routers/Product_Routs.js
@@ -2,17 +2,15 @@ const router = require('express').Router()
 const productCtrl = require('../controllers/Product_controllers')
 const reviewCtrl = require('../controllers/Review_controllers')
 
-
-router.post('/', broductCtrl.AddingProduct)
-router.get('', broductCtrl.GettingAllProducts)
-router.get('/:id', broductCtrl.GettingOneProduct)
-router.put('/:id', broductCtrl.UpdateProduct)
-router.delete('/:id', broductCtrl.DeletingProduct)
+router.post('/', productCtrl.AddingProduct)
+router.get('', productCtrl.GettingAllProducts)
+router.get('/:id', productCtrl.GettingOneProduct)
+router.put('/:id', productCtrl.UpdateProduct)
+router.delete('/:id', productCtrl.DeletingProduct)
 
 //reviews routes
 router.get('/:productId/reviews', reviewCtrl.reviews_getAll)
 router.post('/:productId/reviews', reviewCtrl.reviews_create_post)
 router.delete('/:productId/reviews/:reviewId', reviewCtrl.reviews_delete_delete)
-
 
 module.exports = router
