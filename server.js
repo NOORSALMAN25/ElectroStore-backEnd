015b1e5bb2a6f854edee7749c174c88cf8b2c391
mad6717 <mad6717@gmail.com>
2025-08-11 17:12:44 +0300

    committing what is there

--- server.js
+++ server.js
@@ -32,18 +32,13 @@ app.get('/', (req, res) => {
 const productRouter = require('./Routers/Product_Routs')
 const orderRouter = require('./Routers/Order_Routs')
 const AuthRouter = require('./Routers/AuthRouter')
-
 const profileRouts = require('./Routers/Profile_Routs')
-
 
 // use Routers
 app.use('/products', productRouter)
 app.use('/orders', orderRouter)
 app.use('/auth', AuthRouter)
-
 app.use('/profile', profileRouts)
-
-
 
 // Listener
 app.listen(port, () => {
