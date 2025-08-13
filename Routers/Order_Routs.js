4de9134ae711df6ea8a318d061b5d164fe2223d8
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-13 14:32:09 +0300

    fix the delete review

--- Routers/Order_Routs.js
+++ Routers/Order_Routs.js
@@ -2,10 +2,25 @@ const router = require('express').Router()
 const broductCtrl = require('../controllers/Order_controllers')
 const middleware = require('../middleware/index')
 
-router.post('', broductCtrl.AddingOrder)
+router.post(
+  '',
+  middleware.stripToken,
+  middleware.verifyToken,
+  broductCtrl.AddingOrder
+)
 router.get('', broductCtrl.GettingAllOrder)
 router.get('/:id', broductCtrl.GettingOneOrder)
-router.put('/:id', broductCtrl.UpdateOrder)
-router.delete('/:id', broductCtrl.DeletingOrder)
+router.put(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  broductCtrl.UpdateOrder
+)
+router.delete(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  broductCtrl.DeletingOrder
+)
 
 module.exports = router
