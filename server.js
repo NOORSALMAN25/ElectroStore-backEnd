200d730a1ebe263f501b56e1ec7db747b8c3f7ac
mad6717 <mad6717@gmail.com>
2025-08-16 20:13:25 +0300

    try to fix cors issue

--- server.js
+++ server.js
@@ -21,9 +21,14 @@ app.use(morgan('dev'))
 app.use(
   cors({
     origin: 'https://electro-store.surge.sh',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   })
 )
+// Explicitly handle preflight requests
+app.options('*', cors())
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
